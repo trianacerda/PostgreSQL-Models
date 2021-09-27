1228eb1b453de5b33c830d46bef918e4604ee54e
Triana Cerda <trianaRcerda@gmail.com>
2021-09-27 16:28:02 -0700

    red failing tested for /get route + built app/controllers/models/service/utils files

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,4 +11,17 @@ describe('demo routes', () => {
   afterAll(() => {
     pool.end();
   });
+
+  it('gets pokemon from api', () => {
+    return request(app)
+      .get('/')
+      .then((res) => {
+        console.log('res.body', res.body);
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          pokeName: expect.any(String),
+          url: expect.any(String),
+        });
+      });
+  });
 });
