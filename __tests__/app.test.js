1a70c176dfe18360b24eca385a9fc69eca49d1c0
Triana Cerda <trianaRcerda@gmail.com>
2021-10-01 15:39:31 -0700

    commit for TA to check

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,26 +8,22 @@ describe('demo routes', () => {
     return setup(pool);
   });
 
-  afterAll(() => {
-    pool.end();
-  });
-
-  it('should POST api pokemon to SQL table-- pokemon', () => {
-    return request(app)
-      .post('/api/v2/pokemon')
-      .send({
-        name: 'bulbasaur',
-        url: 'https://pokeapi.co/api/v2/pokemon/1/',
-      })
-      .then((res) => {
-        expect(res.body).toEqual({
-          id: '1',
-          pokeName: 'bulbasaur',
-          url: 'https://pokeapi.co/api/v2/pokemon/1/',
-        });
-      });
-  });
-
+  // it('should POST api pokemon to SQL table-- pokemon', () => {
+  //   return request(app)
+  //     .post('/api/v2/pokemon')
+  //     .send()
+  //     .then((res) => {
+  //       expect(res.body).toEqual(
+  //         expect.arrayContaining[
+  //           expect.objectContaining({
+  //             id: expect.any(String),
+  //             pokeName: expect.any(String),
+  //             url: expect.any(String),
+  //           })
+  //         ]
+  //       );
+  //     });
+  // });
   it.only('should GET ALL pokemon from SQL table', async () => {
     await request(app).post('/api/v2/pokemon').send();
     return request(app)
@@ -35,54 +31,46 @@ describe('demo routes', () => {
       .then((res) => {
         // console.log('!!!!!', res.body);
         expect(res.body).toEqual(expect.any(Array));
-        expect(res.body.length).toEqual(20);
+        expect(res.body.length).toEqual(10);
       });
   });
 
-  it('should GET pokemon by ID', async () => {
-    await request(app).post('/api/v2/pokemon').send({
-      name: 'bulbasaur',
-      url: 'https://pokeapi.co/api/v2/pokemon/1/',
-    });
-    return request(app)
-      .get('/api/v2/pokemon/1')
-      .then((res) => {
-        expect(res.body).toEqual({
-          id: '1',
-          pokeName: 'bulbasaur',
-          url: 'https://pokeapi.co/api/v2/pokemon/1/',
-        });
-      });
-  });
+  // it('should GET pokemon by ID', async () => {
+  //   await request(app).post('/api/v2/pokemon').send({
+  //     name: 'bulbasaur',
+  //     url: 'https://pokeapi.co/api/v2/pokemon/1/',
+  //   });
+  //   return request(app)
+  //     .get('/api/v2/pokemon/1')
+  //     .then((res) => {
+  //       expect(res.body).toEqual({
+  //         id: '1',
+  //         pokeName: 'bulbasaur',
+  //         url: 'https://pokeapi.co/api/v2/pokemon/1/',
+  //       });
+  //     });
+  // });
 
-  it('should PATCH pokemon by ID', async () => {
-    await request(app).post('/api/v2/pokemon').send({
-      name: 'bulbasaur',
-      url: 'https://pokeapi.co/api/v2/pokemon/1/',
-    });
-    await request(app).patch('/api/v2/pokemon/1').send({
-      pokeName: 'TRIANAasaur',
-    });
-    return request(app)
-      .get('/api/v2/pokemon/1')
-      .then((res) => {
-        expect(res.body).toEqual({
-          id: '1',
-          pokeName: 'TRIANAasaur',
-          url: 'https://pokeapi.co/api/v2/pokemon/1/',
-        });
-      });
-  });
+  // it('should PATCH pokemon by ID', async () => {
+  //   await request(app).post('/api/v2/pokemon').send({
+  //     name: 'bulbasaur',
+  //     url: 'https://pokeapi.co/api/v2/pokemon/1/',
+  //   });
+  //   await request(app).patch('/api/v2/pokemon/1').send({
+  //     pokeName: 'TRIANAasaur',
+  //   });
+  //   return request(app)
+  //     .get('/api/v2/pokemon/1')
+  //     .then((res) => {
+  //       expect(res.body).toEqual({
+  //         id: '1',
+  //         pokeName: 'TRIANAasaur',
+  //         url: 'https://pokeapi.co/api/v2/pokemon/1/',
+  //       });
+  //     });
+  // });
 
-  it('should DELETE a pokemon by ID', async () => {
-    await request(app).post('/api/v2/pokemon').send({
-      name: 'bulbasaur',
-      url: 'https://pokeapi.co/api/v2/pokemon/1/',
-    });
-    return request(app)
-      .delete('/api/v2/pokemon/1')
-      .then((res) => {
-        expect(res.body).toEqual({});
-      });
+  afterAll(() => {
+    pool.end();
   });
 });
